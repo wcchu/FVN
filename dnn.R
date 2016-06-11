d3b3057abba11a78e51eb678892f356e8d7ba8b1
wcchu <weichun.chu@gmail.com>
2016-06-11 22:06:02 +0200

    debug

--- dnn.R
+++ dnn.R
@@ -1,61 +1,71 @@
+library(dplyr)
+library(data.table)
 # Input:
 #   x = predictor variables of the reference dataset
 #   y = response of the reference dataset
 #   q = predictor variables of the query dataset
-#   r = normalized radius of the neighborhood in the variable space
+#   d = normalized "effective radius" of the neighborhood in the variable space;
+#       actual radius = sqrt(num_of_variables * d ^ 2)
+#   min.pop = minimum total population in the neighborhood
+#   min.frac = minimum proportion of the most popular class out of the total
+#              population to quarantee that class as the prediction
+#              (only used when response is categorical)
 # Output: predicted response vector
 dNN <- function(x, y, q, d, min.pop = 1, min.frac = 0) {
   # * process training data to create "reference data" for NN alg
   res_type <- ifelse(is.numeric(y), 'numeric', 'class')
+  res_data_type <- ifelse(is.numeric(y), 'numeric', 'character')
   ref <- data.frame(x, dnn.response = y)
-  nv <- ncol(x) # number of variables
-  nr <- nrow(ref) # number of reference points
+  nvar <- ncol(x) # number of variables
+  nref <- nrow(ref) # number of reference points
   # calculate the unit length of each variable
   u <- c()
-  for (j in 1:nv) {
+  for (j in 1:nvar) {
     u[j] <- sd(x[, j])
   }
   # run through queries and predict response for each query
-  nq <- nrow(q)
-  p <- c()
-  for (i in 1:nq) {
-    ss <- c()
-    for (j in 1:nv) {
+  nque <- nrow(q)
+  prediction <- vector(mode = res_data_type, length = nque)
+  comment <- vector(mode = "character", length = nque)
+  n_neighbors <- vector(mode = "integer", length = nque)
+  for (i in 1:nque) {
+    ss <- vector(mode = "numeric", length = nref)
+    for (j in 1:nvar) {
       ss <- ss + ((x[, j] - as.numeric(q[i, j])) / u[j])^2
     }
     distance <- sqrt(ss)
-    y$dist <- distance
+    ref$dist <- distance
     # collect data points in ball
     dball <-
-      y %>%
-      filter(dist <= d)
+      ref %>%
+      dplyr::filter(dist <= sqrt(nvar * d * d))
     n = nrow(dball)
     if (n < min.pop) {
-      pred <- NA
-      status <- "insufficient data points"
+      prediction[i] <- NA
+      comment[i] <- "insufficient data points"
     } else {
       # do statistics in ball
       if (res_type == 'class') {
         sball <-
           dball %>%
-          group_by(y) %>%
-          summarise(nc = n(), frac = nc/n) %>%
-          filter(frac >= min.frac) %>%
-          arrange(desc(frac)) %>%
+          dplyr::group_by(dnn.response) %>%
+          dplyr::summarise(nc = n(), frac = nc/n) %>%
+          dplyr::filter(frac >= min.frac) %>%
+          dplyr::arrange(desc(frac)) %>%
           data.frame()
         if (nrow(sball) == 0) {
-          pred <- NA
-          status <- "fraction of best class too low"
+          prediction[i] <- NA
+          comment[i] <- "fraction of best class too low"
         } else {
-          pred <- sball$y[1]
-          status <- "response predicted: class"
+          prediction[i] <- sball$dnn.response[1]
+          comment[i] <- "categorical response predicted"
         }
       } else if (res_type == 'numeric') {
-        pred <- mean
-        status <- "response predicted: numeric"
+        prediction[i] <- mean(dball$dnn.response, na.rm = T)
+        comment[i] <- "numeric response predicted"
       }
     }
-    p[i] <- pred
+    n_neighbors[i] <- n
   }
-  return(p)
+  return(data.frame(prediction, comment, n_neighbors))
 }
