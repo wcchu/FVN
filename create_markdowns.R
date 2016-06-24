8f7264a396ceb80610b3bdf1630731c4515f2329
Wei-Chun Chu <weichun.chu@gmail.com>
2016-06-24 20:06:28 +0200

    Update and rename dnn_test_markdown.R to create_markdowns.R

--- create_markdowns.R
+++ create_markdowns.R
@@ -2,4 +2,3 @@ require(knitr) # required for knitting from rmd to md
 require(markdown) # required for md to html 
 knit('dnn_test.Rmd', 'dnn_test.md') # creates md file
 markdownToHTML('dnn_test.md', 'dnn_test.html') # creates html file
-
